097b91ba90e5d2109e701b12bd11bc5900db746b
siki <musyajuaayush@gmail.com>
2022-06-30 15:33:31 +0545

    Adds baloon zombie and dragon zombie

--- scripts/classes/Zombie.js
+++ scripts/classes/Zombie.js
@@ -98,7 +98,6 @@ export default class Zombie {
     }
 
     attackAnimation() {
-        console.log("attacking started");
         this.startFrameX = 8;
         this.startFrameY = 2;
         this.endFrameX = 10;
@@ -110,11 +109,12 @@ export default class Zombie {
             let attackRowIdx = this.game.zombiesPositions.indexOf(this.y);
             this.game.zombiesPositions.splice(attackRowIdx, 1);
             this.delete = true;
+            this.game.score += 10;
         }
     }
 
     checkFrames() {
-        if (this.frameY < this.startFrameY) {
+        if (this.frameY < this.startFrameY || this.frameY > this.endFrameY) {
             this.frameX = this.startFrameX;
             this.frameY = this.startFrameY;
         } else if (
@@ -266,5 +266,118 @@ export class ConeHeadZombie extends NormalZombie {
         this.zombieType = new Image();
         this.zombieType.src =
             "../../assets/images/ConeheadZombieSprite_166x144.png";
+    }
+}
+
+export class DragonZombie extends Zombie {
+    initZombieSpecs() {
+        // Movement variables
+        this.velocity = 1;
+        this.increment = this.velocity;
+
+        // Life
+        this.health = 200;
+
+        // Zombie status
+        this.delete = false;
+        this.attacking = false;
+    }
+
+    initZombieAnimation() {
+        // Animation support variables
+        this.startFrameX = 0;
+        this.startFrameY = 0;
+        this.endFrameX = 6;
+        this.endFrameY = 2;
+        this.minFrame = 0;
+        this.maxFrame = 10;
+        this.frameX = this.startFrameX;
+        this.frameY = this.startFrameY;
+        this.spriteW = 464;
+        this.spriteH = 400;
+        this.animationSpeed = 4;
+
+        // Offset for drawing mage
+        this.offsetX = 200;
+        this.offsety = 200;
+    }
+
+    loadSprite() {
+        this.zombieType = new Image();
+        this.zombieType.src = "../../assets/images/dragonZombie_464x400.png";
+    }
+}
+
+export class BallonZombie extends Zombie {
+    initZombieSpecs() {
+        // Movement variables
+        this.velocity = 1;
+        this.increment = this.velocity;
+
+        // Life
+        this.health = 200;
+
+        // Zombie status
+        this.delete = false;
+        this.attacking = false;
+        this.drop = true;
+    }
+
+    initZombieAnimation() {
+        // Animation support variables
+        if (this.drop) {
+            this.startFrameX = 0;
+            this.startFrameY = 0;
+            this.endFrameX = 1;
+            this.endFrameY = 1;
+        } else {
+            this.startFrameX = 7;
+            this.startFrameY = 8;
+            this.endFrameX = 3;
+            this.endFrameY = 12;
+        }
+        this.minFrame = 0;
+        this.maxFrame = 10;
+        this.frameX = this.startFrameX;
+        this.frameY = this.startFrameY;
+        this.spriteW = 207;
+        this.spriteH = 197;
+        this.animationSpeed = 4;
+
+        // Offset for drawing mage
+        this.offsetX = 90;
+        this.offsety = 90;
+    }
+
+    dieAnimation() {
+        this.startFrameX = 1;
+        this.startFrameY = 6;
+        this.endFrameX = 6;
+        this.endFrameY = 8;
+        this.increment = 0;
+    }
+
+    attackAnimation() {
+        if (this.drop) {
+            this.startFrameX = 2;
+            this.startFrameY = 1;
+            this.endFrameX = 7;
+            this.endFrameY = 3;
+            if (this.frameY === this.endFrameY) {
+                this.drop = false;
+            }
+        } else {
+            this.startFrameX = 8;
+            this.startFrameY = 3;
+            this.endFrameX = 0;
+            this.endFrameY = 6;
+            this.checkFrames();
+        }
+    }
+
+    loadSprite() {
+        this.zombieType = new Image();
+        this.zombieType.src =
+            "../../assets/images/ballonZombieSprite_207x197.png";
     }
 }
